427f74095b8d52a83b163434108c74e34700ea54
Saleh Alhaddad <salehalhaddad010@gmail.com>
2021-09-09 08:58:40 +0300

    solved roman challenges

--- roman_numerals_encoder.rb
+++ roman_numerals_encoder.rb
@@ -2,6 +2,7 @@
 
 # Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
 
+
 # Example:
 
 # solution(1000) # should return 'M'
@@ -79,3 +80,4 @@ def solution(number)
   end
 end
 ------------------------------------------
+  
